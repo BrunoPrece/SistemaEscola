4d77064cb0df46e864beecbeec8944a6a8bc8ad2
Fernando <Fernando@DESKTOP-BEEJG72>
2016-06-04 11:33:32 -0300

    Relacionamento N:N FODA =/

--- SistemaEscola/src/br/com/escola/entity/Turma.java
+++ SistemaEscola/src/br/com/escola/entity/Turma.java
@@ -1,54 +1,64 @@
 package br.com.escola.entity;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
  * Classe que representa a entidade turma.
+ *
  * @author Fernando Ortiz
  */
 @Entity
 @Table(name = "tab_turma")
 public class Turma implements Serializable {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int codTurma;
-    
-   // @ManyToMany
-    private Professor professor;    
-     
+
     @ManyToOne
     private Curso curso;
-    
+
     @Column
     private String nome;
-    
-    public Turma(){
-        
+
+    @ManyToMany
+    @JoinTable(name = "professor_has_turma", joinColumns
+            = {
+                @JoinColumn(name = "codTurma")}, inverseJoinColumns
+            = {
+                @JoinColumn(name = "id")})
+    private List<Professor> professor;
+  
+    public Turma() {
+
     }
 
+     public List<Professor> getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(List<Professor> professor) {
+        this.professor = professor;
+    }
+    
+    
     public int getCodTurma() {
         return codTurma;
     }
 
     public void setCodTurma(int codTurma) {
         this.codTurma = codTurma;
-    }
-
-    public Professor getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(Professor professor) {
-        this.professor = professor;
     }
 
     public String getNome() {
@@ -66,7 +76,5 @@ public class Turma implements Serializable {
     public void setCurso(Curso curso) {
         this.curso = curso;
     }
-    
-    
-    
+
 }
